b30fe2b2a13ba4b77362fa006728d08391e053ae
lichy <lichunyangcoder@gmail.com>
2020-08-22 13:47:52 +0800

    change setting

--- SpaceShop/settings/production.py
+++ SpaceShop/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
-SECRET_KEY = os.environ['SPACE_SHOP_SECRET_KEY']
-DB_PASSWORD = os.environ['SPACE_SHOP_DB_PASSWORD']
+# SECRET_KEY = os.environ['SPACE_SHOP_SECRET_KEY']
+# DB_PASSWORD = os.environ['SPACE_SHOP_DB_PASSWORD']
 
 DATABASES = {
     'default': {
@@ -10,10 +10,13 @@ DATABASES = {
         'HOST': '81.70.37.90',
         'PORT': 3306,
         'USER': 'root',
-        'PASSWORD': DB_PASSWORD
+        'PASSWORD': 123456
     }
 }
 
-YUNPIAN_APIKEY = os.environ['SPACE_SHOP_YUNPIAN_APIKEY']
+SECRET_KEY = 'g5q!dsx7wh^cv)40_lix&k5u3p7=bq0r#)-oq7ytxk@_3l8-uk'
+
+YUNPIAN_APIKEY = "756f2d59ff694f7f8ea3ea027dd9492c"
+# YUNPIAN_APIKEY = os.environ['SPACE_SHOP_YUNPIAN_APIKEY']
 YUNPIAN_TEXT = "【李春杨】您的验证码是{}。如非本人操作，请忽略本短信"
 
