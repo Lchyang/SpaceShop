7da686476368ae15e36cb8bf1c7c429f8ac222b9
lichy <lichunyangcoder@gmail.com>
2020-08-24 23:49:27 +0800

    add celery

--- SpaceShop/settings/base.py
+++ SpaceShop/settings/base.py
@@ -162,3 +162,10 @@ RETURN_URL = "http://81.70.37.90:8081/alipay/return"
 NOTIFY_URL = "http://81.70.37.90:8081/alipay/return"
 
 APP_ID = "2016102600761595"
+
+# celery
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
