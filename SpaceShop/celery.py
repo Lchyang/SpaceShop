10d5f63b10040e76b826963aa1545da3ababb55c
lichy <lichunyangcoder@gmail.com>
2020-08-30 23:20:45 +0800

    add django-celery

--- SpaceShop/celery.py
+++ SpaceShop/celery.py
@@ -4,5 +4,17 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SpaceShop.settings.local')
 
 celery_app = Celery('SpaceShop')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
 celery_app.autodiscover_tasks()
+
+
+@celery_app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
